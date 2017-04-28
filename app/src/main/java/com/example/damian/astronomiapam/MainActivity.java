6a34d7dd00a31221725044f4ed0de5f9185f7fc3
Damian <ja@me.com>
2017-04-28 22:56:07 +0200

    dodano zegarek odswierzajacy sie co sekunde, przycisk do ustawien. wsyztsko to po za fragmentami we fragmentActivity

--- app/src/main/java/com/example/damian/astronomiapam/MainActivity.java
+++ app/src/main/java/com/example/damian/astronomiapam/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.damian.astronomiapam;
 
 import android.content.Intent;
+import android.icu.util.Calendar;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
@@ -9,6 +12,8 @@ import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import static com.example.damian.astronomiapam.SettingsActivity.EXTRA_MESSAGE_D;
 import static com.example.damian.astronomiapam.SettingsActivity.EXTRA_MESSAGE_SZ;
@@ -18,6 +23,10 @@ public class MainActivity extends FragmentActivity {
 
     public double dlugosc = 0;
     public double szerokosc = 0;
+
+    TextView Time;
+    TextView longtitude;
+    TextView latitude;
     /**
      * The number of pages (wizard steps) to show in this demo.
      */
@@ -34,6 +43,7 @@ public class MainActivity extends FragmentActivity {
      */
     private PagerAdapter mPagerAdapter;
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,6 +51,32 @@ public class MainActivity extends FragmentActivity {
         Intent SettingsIntent = getIntent();
         dlugosc = Double.parseDouble(SettingsIntent.getStringExtra(EXTRA_MESSAGE_D));
         szerokosc = Double.parseDouble(SettingsIntent.getStringExtra(EXTRA_MESSAGE_SZ));
+
+        Time = (TextView) findViewById(R.id.Time);
+        longtitude = (TextView) findViewById(R.id.Longitude);
+        latitude = (TextView) findViewById(R.id.Latitude);
+
+        Thread t = new Thread() {
+
+            @Override
+            public void run() {
+                try {
+                    while (!isInterrupted()) {
+                        Thread.sleep(1000);
+                        runOnUiThread(new Runnable() {
+                            Calendar c = Calendar.getInstance();
+                            @Override
+                            public void run() {
+                                Time.setText("Czas: " + c.get(Calendar.HOUR) + ":" + c.get(Calendar.MINUTE) + ":" + c.get(Calendar.SECOND) );
+                            }
+                        });
+                    }
+                } catch (InterruptedException e) {
+                }
+            }
+        };
+
+        t.start();
 
         // Instantiate a ViewPager and a PagerAdapter.
         mPager = (ViewPager) findViewById(R.id.pager);
@@ -59,6 +95,10 @@ public class MainActivity extends FragmentActivity {
 //            // Otherwise, select the previous step.
 //            mPager.setCurrentItem(mPager.getCurrentItem() - 1);
 //        }
+    }
+
+    public void SettingsClicked(View view) {
+        this.onBackPressed();
     }
 
     /**
--- app/src/main/java/com/example/damian/astronomiapam/MoonFragment.java
+++ app/src/main/java/com/example/damian/astronomiapam/MoonFragment.java
@@ -1,22 +1,55 @@
 package com.example.damian.astronomiapam;
 
+import android.annotation.TargetApi;
 import android.content.Context;
+import android.icu.util.Calendar;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.astrocalculator.AstroCalculator;
+import com.astrocalculator.AstroDateTime;
 
 
 public class MoonFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    TextView wschTextVMoon;
+    TextView zachTextVMoon;
+    TextView NajNowTextVMoon;
+    TextView NajPelniaTextVMoon;
+    TextView FazaKTextVMoon;
+    TextView DzienMieSynodTextVMoon;
+
+    @TargetApi(Build.VERSION_CODES.N)
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(
                 R.layout.fragment_moon, container, false);
+        wschTextVMoon = (TextView) rootView.findViewById(R.id.wschTextVMoon);
+        zachTextVMoon = (TextView) rootView.findViewById(R.id.zachTextVMoon);
+        NajNowTextVMoon = (TextView) rootView.findViewById(R.id.NajNowTextVMoon);
+        NajPelniaTextVMoon = (TextView) rootView.findViewById(R.id.NajPelniaTextVMoon);
+        FazaKTextVMoon = (TextView) rootView.findViewById(R.id.FazaKTextVMoon);
+        DzienMieSynodTextVMoon = (TextView) rootView.findViewById(R.id.DzienMieSynodTextVMoon);
+
+        Calendar cal = Calendar.getInstance();
+        AstroDateTime AstroDT = new AstroDateTime(cal.get(Calendar.YEAR),cal.get(Calendar.MONTH)+1,cal.get(Calendar.DAY_OF_MONTH),cal.get(Calendar.HOUR),cal.get(Calendar.MINUTE),cal.get(Calendar.SECOND),1,false);
+        Bundle bundle = new Bundle();
+        AstroCalculator astroCalculator = new AstroCalculator(AstroDT, new AstroCalculator.Location(bundle.getDouble("dlugosc"),bundle.getDouble("szerokosc")));
+
+        wschTextVMoon.setText("Wschod: " + astroCalculator.getMoonInfo().getMoonset());
+        zachTextVMoon.setText("Zachod: " + astroCalculator.getMoonInfo().getMoonrise());
+        NajNowTextVMoon.setText("Najblizszy now: " + astroCalculator.getMoonInfo().getNextNewMoon());
+        NajPelniaTextVMoon.setText("Najblizsza pelnia: " + astroCalculator.getMoonInfo().getNextFullMoon());
+        FazaKTextVMoon.setText("Faza ksiezyca: " + String.format("%.2f", astroCalculator.getMoonInfo().getIllumination()) + "%");
+        DzienMieSynodTextVMoon.setText("Dzien miesiaca Synodycznego: " + (Math.abs(astroCalculator.getMoonInfo().getNextNewMoon().getDay() - cal.get(Calendar.DAY_OF_MONTH))));
 
         return rootView;
     }
