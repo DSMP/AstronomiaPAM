ab5db92f40aaed848d41a59b48ebed2ec74e4731
Damian <ja@me.com>
2017-05-01 19:36:09 +0200

    zrobione layouty phone+tablet

--- app/src/main/java/com/example/damian/astronomiapam/MainActivity.java
+++ app/src/main/java/com/example/damian/astronomiapam/MainActivity.java
@@ -8,15 +8,12 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
-import static android.R.attr.action;
 import static com.example.damian.astronomiapam.SettingsActivity.EXTRA_MESSAGE_D;
 import static com.example.damian.astronomiapam.SettingsActivity.EXTRA_MESSAGE_R;
 import static com.example.damian.astronomiapam.SettingsActivity.EXTRA_MESSAGE_SZ;
