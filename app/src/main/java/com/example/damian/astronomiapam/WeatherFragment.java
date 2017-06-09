0fb6d6dee882f311070b3271e8b885c94b35d665
Damian <ja@me.com>
2017-06-09 10:25:58 +0200

    wyjebalem z fragmentu wszystko

--- app/src/main/java/com/example/damian/astronomiapam/WeatherFragment.java
+++ app/src/main/java/com/example/damian/astronomiapam/WeatherFragment.java
@@ -40,16 +40,7 @@ import com.example.damian.astronomiapam.service.YahooWeatherService;
 // * Use the {@link WeatherFragment#newInstance} factory method to
 // * create an instance of this fragment.
 // */
-public class WeatherFragment extends Fragment implements WeatherServiceListener, GeocodingServiceListener, LocationListener {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
+public class WeatherFragment extends Fragment {
 
 
     public static int GET_WEATHER_FROM_CURRENT_LOCATION = 0x00001;
@@ -59,63 +50,17 @@ public class WeatherFragment extends Fragment implements WeatherServiceListener,
     private TextView conditionTextView;
     private TextView locationTextView;
 
-    private YahooWeatherService weatherService;
-    private GoogleMapsGeocodingService geocodingService;
-    private WeatherCacheService cacheService;
-
-    private ProgressDialog loadingDialog;
-
-    // weather service fail flag
-    private boolean weatherServicesHasFailed = false;
-
-    private SharedPreferences preferences = null;
-
-
-
 //    private OnFragmentInteractionListener mListener;
 
     public WeatherFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-//     *
-//     * @param param1 Parameter 1.
-//     * @param param2 Parameter 2.
-     * @return A new instance of fragment WeatherFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-//    public static WeatherFragment newInstance(String param1, String param2) {
-//        WeatherFragment fragment = new WeatherFragment();
-//        Bundle args = new Bundle();
-//        args.putString(ARG_PARAM1, param1);
-//        args.putString(ARG_PARAM2, param2);
-//        fragment.setArguments(args);
-//        return fragment;
-//    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        if (getArguments() != null) {
-//            mParam1 = getArguments().getString(ARG_PARAM1);
-//            mParam2 = getArguments().getString(ARG_PARAM2);
-//        }
 
-
-        preferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
-
-        weatherService = new YahooWeatherService(this);
-        weatherService.setTemperatureUnit(preferences.getString(getString(R.string.pref_temperature_unit), null));
-
-        geocodingService = new GoogleMapsGeocodingService(this);
-        cacheService = new WeatherCacheService(getActivity());
-
-//        if (preferences.getBoolean(getString(R.string.pref_needs_setup), true)) {
-//            startSettingsActivity();
-//        }
     }
 
     @Override
@@ -129,188 +74,14 @@ public class WeatherFragment extends Fragment implements WeatherServiceListener,
         conditionTextView = (TextView) rootView.findViewById(R.id.conditionTextView);
         locationTextView = (TextView) rootView.findViewById(R.id.locationTextView);
         // Inflate the layout for this fragment
-
-        Bundle bundle = new Bundle();
-        double a = bundle.getDouble("szerokosc");
-        locationTextView.setText(bundle.getString("lokacja"));
-
-
-        getWeatherFromCurrentLocation();
         return rootView;
     }
     @Override
     public void onStart() {
         super.onStart();
-
-//        loadingDialog = new ProgressDialog(getActivity().getApplicationContext());
-//        loadingDialog.setMessage(getString(R.string.loading));
-//        loadingDialog.setCancelable(false);
-//        loadingDialog.show();
-
-        String location = null;
-
-        getWeatherFromCurrentLocation();
-//        if (preferences.getBoolean(getString(R.string.pref_geolocation_enabled), true)) {
-//            String locationCache = preferences.getString(getString(R.string.pref_cached_location), null);
-//
-//            if (locationCache == null) {
-//                getWeatherFromCurrentLocation();
-//            } else {
-//                location = locationCache;
-//            }
-//        } else {
-//            location = preferences.getString(getString(R.string.pref_manual_location), null);
-//        }
-//
-//        if (location != null) {
-//            weatherService.refreshWeather(location);
-//        }
-    }
-    // TODO: Rename method, update argument and hook method into UI event
-//    public void onButtonPressed(Uri uri) {
-//        if (mListener != null) {
-//            mListener.onFragmentInteraction(uri);
-//        }
-//    }
-    private void getWeatherFromCurrentLocation() {
-        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(getActivity(), new String[]{
-                    Manifest.permission.ACCESS_FINE_LOCATION,
-            }, GET_WEATHER_FROM_CURRENT_LOCATION);
-
-            return;
-        }
-
-        // system's LocationManager
-        final LocationManager locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
-        boolean isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
-        boolean isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
-
-        Criteria locationCriteria = new Criteria();
-
-        if (isNetworkEnabled) {
-            locationCriteria.setAccuracy(Criteria.ACCURACY_COARSE);
-        } else if (isGPSEnabled) {
-            locationCriteria.setAccuracy(Criteria.ACCURACY_FINE);
-        }
-
-        locationManager.requestSingleUpdate(locationCriteria, this, null);
+        Bundle bundle = new Bundle();
+        double a = bundle.getDouble("szerokosc");
+        locationTextView.setText(bundle.getString("lokacja"));
     }
 
-    @Override
-    public void serviceSuccess(Channel channel) {
-        loadingDialog.hide();
-
-        Condition condition = channel.getItem().getCondition();
-        Units units = channel.getUnits();
-        Condition[] forecast = channel.getItem().getForecast();
-
-        int weatherIconImageResource = getResources().getIdentifier("icon_" + condition.getCode(), "drawable", getActivity().getPackageName());
-
-        weatherIconImageView.setImageResource(weatherIconImageResource);
-        temperatureTextView.setText(getString(R.string.temperature_output, condition.getTemperature(), units.getTemperature()));
-        conditionTextView.setText(condition.getDescription());
-        locationTextView.setText(channel.getLocation());
-
-        for (int day = 0; day < forecast.length; day++) {
-            if (day >= 5) {
-                break;
-            }
-
-            Condition currentCondition = forecast[day];
-
-            int viewId = getResources().getIdentifier("forecast_" + day, "id", getActivity().getPackageName());
-            WeatherConditionFragment fragment = (WeatherConditionFragment) getActivity().getSupportFragmentManager().findFragmentById(viewId);
-
-            if (fragment != null) {
-                fragment.loadForecast(currentCondition, channel.getUnits());
-            }
-        }
-
-        cacheService.save(channel);
-    }
-
-    @Override
-    public void serviceFailure(Exception exception) {
-        // display error if this is the second failure
-        if (weatherServicesHasFailed) {
-            loadingDialog.hide();
-            Toast.makeText(getActivity(), exception.getMessage(), Toast.LENGTH_LONG).show();
-        } else {
-            // error doing reverse geocoding, load weather data from cache
-            weatherServicesHasFailed = true;
-            // OPTIONAL: let the user know an error has occurred then fallback to the cached data
-            Toast.makeText(getActivity(), exception.getMessage(), Toast.LENGTH_SHORT).show();
-
-            cacheService.load(this);
-        }
-    }
-
-    @Override
-    public void geocodeSuccess(LocationResult location) {
-        // completed geocoding successfully
-        weatherService.refreshWeather(location.getAddress());
-
-        SharedPreferences.Editor editor = preferences.edit();
-        editor.putString(getString(R.string.pref_cached_location), location.getAddress());
-        editor.apply();
-    }
-
-    @Override
-    public void geocodeFailure(Exception exception) {
-        // GeoCoding failed, try loading weather data from the cache
-        cacheService.load(this);
-    }
-
-    @Override
-    public void onLocationChanged(Location location) {
-        geocodingService.refreshLocation(location);
-    }
-
-    @Override
-    public void onStatusChanged(String s, int i, Bundle bundle) {
-        // OPTIONAL: implement your custom logic here
-    }
-
-    @Override
-    public void onProviderEnabled(String s) {
-        // OPTIONAL: implement your custom logic here
-    }
-
-    @Override
-    public void onProviderDisabled(String s) {
-        // OPTIONAL: implement your custom logic here
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-//    public interface OnFragmentInteractionListener {
-//        // TODO: Update argument type and name
-//        void onFragmentInteraction(Uri uri);
-//    }
-
-//    @Override
-//    public void onAttach(Context context) {
-//        super.onAttach(context);
-//        if (context instanceof OnFragmentInteractionListener) {
-//            mListener = (OnFragmentInteractionListener) context;
-//        } else {
-//            throw new RuntimeException(context.toString()
-//                    + " must implement OnFragmentInteractionListener");
-//        }
-//    }
-//
-//    @Override
-//    public void onDetach() {
-//        super.onDetach();
-//        mListener = null;
-//    }
 }
